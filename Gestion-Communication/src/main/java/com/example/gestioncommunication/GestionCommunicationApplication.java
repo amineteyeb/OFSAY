8ab9fff07d630a112647be3742c0caa4fd69e909
yara ourabi <ourabi.yara@esprit.tn>
2023-10-23 00:22:35 +0100

    commit

---
+++ Gestion-Communication/src/main/java/com/example/gestioncommunication/GestionCommunicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestioncommunication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCommunicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionCommunicationApplication.class, args);
+	}
+
+}
